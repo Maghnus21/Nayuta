7eb4ad72168db0c8294ba060f6e889cd75e931b0
Maghnus21 <maghnusodonnell@outlook.com>
2024-03-15 20:00:24 +0000

    cleaning player script. fixed cast_ray func, moved object pickup to seperate func, started work on last used weapon and hot swapping functionality

--- Player/scripts/player.gd
+++ Player/scripts/player.gd
@@ -61,7 +61,7 @@ var time_passed:float = 0.0
 
 
 enum weapons {UNARMED, PISTOL, SMG, SHOTGUN}
-
+var last_used_weap
 
 
 # Called when the node enters the scene tree for the first time.
@@ -79,103 +79,103 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	
-	
 	player_attack(delta)
 	change_weapon()
+	pick_up_obj()
 	
-	if Input.is_action_just_pressed("interact_key"):
-		if held_obj != null:
-			drop_obj()
-		else:
-			cast_ray(5.0)
-	
-	if Input.is_action_just_pressed("mouse_left") && held_obj != null:
-		throw_obj()
 
 
 
 func _physics_process(delta):
 	if held_obj != null:
-		pickup()
+		hold_obj()
 	
 	#check_health()
 	pass
 
+##	casts ray from center of screen to specified units forward
 func cast_ray(range:float):
 	var space = get_world_3d().direct_space_state
 	var query = PhysicsRayQueryParameters3D.create(camera.global_position - camera.global_transform.basis.z * 0.6, camera.global_position - camera.global_transform.basis.z * range)
 	query.hit_from_inside = false
-		
+	query.hit_back_faces = false
+	
 	var collision = space.intersect_ray(query)
 	
-	#return collision
+	return collision
+
+##	checks if player has pressed interact key currently "F" and picks up rigidbody object
+func pick_up_obj():
+	if Input.is_action_just_pressed("interact_key"):
+		if held_obj != null:
+			drop_obj()
+		else:
+			pickup(cast_ray(5.0))	# calls cast_ray to return PhysicsRayQueryParameter3D data and is set to pickuk func
 	
+	if Input.is_action_just_pressed("mouse_left") && held_obj != null:
+		throw_obj()
+	
+	pass
+
+##	uses data from "cast_ray()" func and checks node group. if meets requirements, picks up object
+func pickup(collision):
 	if collision:
 		print("hit obj: ", collision.collider.name)
 		if collision.collider.is_in_group("holdable"):
+			change_weapon_by_int(weapons.UNARMED)
+			
 			held_obj = collision.collider
 			held_obj.global_rotation = pickup_point.global_rotation
-			#held_obj.lock_rotation = true
+	pass
 
-func pickup():
-	#held_obj.global_position = lerp(held_obj.global_position, pickup_point.global_position, 0.5)
+##	
+func hold_obj():
 	held_obj.set_linear_velocity((pickup_point.global_transform.origin - held_obj.global_transform.origin) * PULL_FORCE)
 	pass
 
 func drop_obj():
+	change_weapon_by_int(last_used_weap)
+	
 	held_obj.lock_rotation = false
 	held_obj = null
 
 func throw_obj():
+	change_weapon_by_int(last_used_weap)
 	
 	var forward = -camera.global_transform.basis.z
 	
 	held_obj.apply_impulse(forward * THROW_FORCE)
 	drop_obj()
-	
-	pass
 
 func fire_shotgun():
-	raycast.force_raycast_update()
-	var ray_endpoint = raycast.get_collision_point()
-	
 	var col = cast_ray(1000)
 	
-	shotgun_barrel.look_at(ray_endpoint,  Vector3.UP, true)
+	shotgun_barrel.look_at(col.position,  Vector3.UP, true)
 	
 	gun_source.stream = shotgun_audio
 	gun_source.play()
 	
 	shoot_bullets(shotgun_barrel, 10, Vector3(shotgun_spread,shotgun_spread,0))
-	pass
 
 func fire_pistol():
-	raycast.force_raycast_update()
-	var ray_endpoint = raycast.get_collision_point()
-	
 	var col = cast_ray(1000)
 	
-	pistol_barrel.look_at(ray_endpoint,  Vector3.UP, true)
+	pistol_barrel.look_at(col.position,  Vector3.UP, true)
 	
 	gun_source.stream = pistol_audio
 	gun_source.play()
 	
 	shoot_bullets(pistol_barrel, 1, Vector3(pistol_spread,pistol_spread,0))
-	pass
 
 func fire_smg():
-	raycast.force_raycast_update()
-	var ray_endpoint = raycast.get_collision_point()
-	
 	var col = cast_ray(1000)
 	
-	smg_barrel.look_at(ray_endpoint,  Vector3.UP, true)
+	smg_barrel.look_at(col.position,  Vector3.UP, true)
 	
 	gun_source.stream = smg_audio
 	gun_source.play()
 	
 	shoot_bullets(smg_barrel, 1, Vector3(smg_spread,smg_spread,0))
-	pass
 
 func rebound():
 	pass
@@ -192,9 +192,6 @@ func shoot_bullets(bullet_spawn:Node3D, bullet_count:int, spread:Vector3):
 		#print("bullet pos: ", instance.global_position, "\t spawn pos", bullet_spawn.global_position)
 		instance.rotation = bullet_spawn.global_rotation + Vector3(x_mod, y_mod, 0)
 		get_parent().get_parent().add_child(instance)
-		
-		#rng.seed(t.get_ticks_msec() + x_mod)
-	
 	
 	pass
 
@@ -218,6 +215,8 @@ func player_attack(delta):
 
 func  change_weapon():
 	if Input.is_action_just_pressed("alpha_1"):
+		last_used_weap = active_weapon
+		
 		active_weapon = weapons.PISTOL
 		rate_of_fire = 60.0 / pistol_rpm
 		time_passed = 0.0
@@ -226,6 +225,8 @@ func  change_weapon():
 		smg.visible = false
 		shotgun.visible = false
 	if Input.is_action_just_pressed("alpha_2"):
+		last_used_weap = active_weapon
+		
 		active_weapon = weapons.SMG
 		rate_of_fire = 60.0 / smg_rpm
 		time_passed = 0.0
@@ -234,6 +235,8 @@ func  change_weapon():
 		smg.visible = true
 		shotgun.visible = false
 	if Input.is_action_just_pressed("alpha_3"):
+		last_used_weap = active_weapon
+		
 		active_weapon = weapons.SHOTGUN
 		rate_of_fire = 60.0 / shotgun_rpm
 		time_passed = 0.0
@@ -241,6 +244,57 @@ func  change_weapon():
 		pistol.visible = false
 		smg.visible = false
 		shotgun.visible = true
+
+func change_weapon_by_int(new_weapon):
+	match new_weapon:
+		#	UNARMED
+		1: 
+			last_used_weap = active_weapon
+			print(last_used_weap)
+			active_weapon = weapons.UNARMED
+			
+			pistol.visible = false
+			smg.visible = false
+			shotgun.visible = false
+			
+		
+		#	pistol
+		2:
+			last_used_weap = active_weapon
+			
+			active_weapon = weapons.PISTOL
+			rate_of_fire = 60.0 / pistol_rpm
+			time_passed = 0.0
+		
+			pistol.visible = true
+			smg.visible = false
+			shotgun.visible = false
+		
+		#	smg
+		3:
+			last_used_weap = active_weapon
+			
+			active_weapon = weapons.SMG
+			rate_of_fire = 60.0 / smg_rpm
+			time_passed = 0.0
+		
+			pistol.visible = false
+			smg.visible = true
+			shotgun.visible = false
+		
+		#	shotgun
+		4:
+			last_used_weap = active_weapon
+			
+			active_weapon = weapons.SHOTGUN
+			rate_of_fire = 60.0 / shotgun_rpm
+			time_passed = 0.0
+		
+			pistol.visible = false
+			smg.visible = false
+			shotgun.visible = true
+	pass
+
 
 func damage(damage_amount):
 	print(health)
